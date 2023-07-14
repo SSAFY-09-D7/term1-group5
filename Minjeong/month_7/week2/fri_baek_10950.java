61b074187c77110c0470a0fd53840a99d0c33355
권민정 <22alswjd@naver.com>
2023-07-14 20:54:59 +0900

    baek_10950_A+B-3

---
+++ Minjeong/month_7/week2/fri_baek_10950.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class fri_baek_10950 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
